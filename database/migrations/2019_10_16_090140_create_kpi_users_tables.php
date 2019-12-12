332f174bea7baac229294f0e43f4fe1674007b1d
Tran Huy <huytran161297@gmail.com>
2019-12-12 17:11:38 +0700

    remove code

--- database/migrations/2019_10_16_090140_create_kpi_users_tables.php
+++ database/migrations/2019_10_16_090140_create_kpi_users_tables.php
@@ -13,70 +13,6 @@ class CreateKpiUsersTables extends Migration
      */
     public function up()
     {
-        Schema::create('kpis', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('position');
-            $table->integer('kpi');
-            $table->timestamps();
-        });
-
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('phone');
-            $table->string('address');
-            $table->string('birth');
-            $table->string('department');
-            $table->string('id_position');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('kpi_users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('id_user')->unsigned();
-            $table->string('complete_tasks');
-            $table->integer('rate1');
-            $table->string('working_hours');
-            $table->integer('rate2');
-            $table->string('time_late');
-            $table->integer('rate3');
-            $table->integer('month');
-            $table->integer('year');
-            $table->timestamps();
-        });
-
-        Schema::create('evaluate', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('profits');
-            $table->timestamps();
-        });
-
-        Schema::create('department', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('id_evaluate');
-            $table->timestamps();
-        });
-
-        Schema::create('kpi_department', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('id_department')->unsigned();
-            $table->integer('profits');
-            $table->integer('month');
-            $table->integer('year');
-            $table->timestamps();
-        });
-
         Schema::create('role', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('id_employee');
@@ -92,20 +28,6 @@ class CreateKpiUsersTables extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('kpis');
-
-        Schema::dropIfExists('users');
-
-        Schema::dropIfExists('password_resets');
-
-        Schema::dropIfExists('kpi_users');
-
-        Schema::dropIfExists('evaluate');
-
-        Schema::dropIfExists('department');
-
-        Schema::dropIfExists('kpi_department');
-
         Schema::dropIfExists('role');
     }
 }
